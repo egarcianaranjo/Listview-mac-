130958d2f8fda26569b90d653ee73d9c28fec039
Emilio Garcia Naranjo <egarcianaranjo@gmail.com>
2015-11-18 01:09:50 +0100

    Ejemplo de listview con un solo activity

--- Listview(mac)/app/src/main/java/com/example/emiliogarcianaranjo/listviewmac/MainActivity.java
+++ Listview(mac)/app/src/main/java/com/example/emiliogarcianaranjo/listviewmac/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.emiliogarcianaranjo.listviewmac;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends ActionBarActivity {
+
+    private ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lista = (ListView)findViewById(R.id.lvLista);
+
+        // Array con las opciones
+        final String[] datos = new String[]{
+                "Opcion 1",
+                "Opcion 2",
+                "Opcion 3",
+                "Opcion 4",
+                "Opcion 5",
+        };
+
+        // Adaptador
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datos);
+
+        // Establecemos el adaptador del listview
+        lista.setAdapter(adaptador);
     }
 }
